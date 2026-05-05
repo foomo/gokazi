9f89707139e08f32151221e494407a44e4ab2db6
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-05-05 16:52:00 +0200

    refactor(cmd): add New() constructor for full command tree

---
+++ cmd/build.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"log/slog"
+
+	"github.com/spf13/cobra"
+)
+
+// New returns the fully-wired gokazi root command with all subcommands attached.
+// Reused by the binary entry point and by cmd/docgen.
+func New(l *slog.Logger) *cobra.Command {
+	root := NewRoot(l)
+	root.AddCommand(
+		NewList(l),
+		NewStop(l),
+		NewConfig(l),
+		NewVersion(l),
+	)
+	return root
+}
